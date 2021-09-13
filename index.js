7cbba903aefa728a904e171070894a804abe2325
Malith-Anjana <malithanjana97@gmail.com>
2021-09-13 13:56:41 +0530

    Navigation Buttons Add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import main from './src/main.js';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => main);
---
+++ src/main.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Home = ({navigation}) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.bodyText}>Welcome to Home</Text>
+      <Button
+        title="go To page2"
+        onPress={() => navigation.navigate('Page 02')}
+      />
+    </View>
+  );
+};
+
+const Page2 = ({navigation}) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.bodyText}>Welcome to Page2</Text>
+
+      <Button
+        title="Go to whatever 1st page"
+        onPress={() => navigation.popToTop()}
+      />
+
+      <Button title="Previous Page" onPress={() => navigation.goBack()} />
+
+      <Button title="Refresh" onPress={() => navigation.push('Page 02')} />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+class main extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Page 02" component={Page2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  bodyText: {
+    fontSize: 30,
+  },
+});
+
+export default main;
